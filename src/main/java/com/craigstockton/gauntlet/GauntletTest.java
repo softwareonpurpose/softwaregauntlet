38bc385d2541e19e82cd8d311d53182e585f080c
Craig A. Stockton <craig.a.stockton@gmail.com>
2015-09-22 14:41:18 -0700

    Renamed method getBrowser() to getUiHost()

--- src/main/java/com/craigstockton/gauntlet/GauntletTest.java
+++ src/main/java/com/craigstockton/gauntlet/GauntletTest.java
@@ -83,7 +83,7 @@ public abstract class GauntletTest {
         UiHost.quitInstance();
     }
 
-    public static UiHost getBrowser() {
+    public static UiHost getUiHost() {
         return UiHost.getInstance();
     }
 
